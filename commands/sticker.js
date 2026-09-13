edc56af2875100cf2eababd99e6fd2d403147863
Simon tech <officialsimondon@gmail.com>
2026-09-14 00:41:22 +0100

    Create sticker command menu in sticker.js
    Add sticker command menu with various options.

---
+++ commands/sticker.js
@@ -0,0 +1,23 @@
+🌍⃝⃘‌‌‌━⋆─⋆──❂
+┊ ┊ ┊ ┊ ┊
+┊ ┊ ✫ ˚㋛ ⋆｡ ❀
+┊ ☠︎︎
+✧  x sticker 𓂃✍︎𝄞
+╰────────────────❂
+
+┏━━━━━━━━━━━━━❥❥❥
+┃      🎨 STICKER MENU
+┗━━━━━━━━━━━━━❥❥❥
+
+┃ 01. x sticker
+┃ 02. x s
+┃ 03. x stickerwm
+┃ 04. x take
+┃ 05. x steal
+┃ 06. x toimg
+┃ 07. x tomeme
+┃ 08. x attp
+┃ 09. x tgs
+┃ 10. x emojimix
+
+> _🎨 Send/reply to media with a sticker command_
