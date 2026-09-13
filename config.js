79f197e29b473487736aecf1edc7e39947aafef1
Simon tech <officialsimondon@gmail.com>
2026-09-14 00:54:04 +0100

    Change bot command prefix to 'x'
    Updated bot command prefix from '.' to 'x'.

--- config.js
+++ config.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 
 module.exports = {
-  botName: process.env.BOT_NAME || 'X Ban',
-  prefix: process.env.PREFIX || '.',
-  phoneNumber: process.env.PHONE_NUMBER,
-  ownerNumber: process.env.OWNER_NUMBER,
-  sessionFolder: process.env.SESSION_FOLDER || './auth_info',
-  ownerJid: process.env.OWNER_NUMBER
-    ? process.env.OWNER_NUMBER.replace(/\D/g, '') + '@s.whatsapp.net'
-    : null,
+    botName: process.env.BOT_NAME || 'X Ban',
+
+    // Bot command prefix
+    prefix: process.env.PREFIX || 'x',
+
+    // WhatsApp number used for pairing
+    phoneNumber: process.env.PHONE_NUMBER,
+
+    // Owner's WhatsApp number
+    ownerNumber: process.env.OWNER_NUMBER,
+
+    // Session storage
+    sessionFolder: process.env.SESSION_FOLDER || './auth_info',
+
+    // Owner JID
+    ownerJid: process.env.OWNER_NUMBER
+        ? process.env.OWNER_NUMBER.replace(/\D/g, '') + '@s.whatsapp.net'
+        : null
 };
